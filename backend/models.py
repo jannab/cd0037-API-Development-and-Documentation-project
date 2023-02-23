2d1e9a0d0500c0ce7879259eba4824265c1198ed
Janna Brettingen <janna@brettingen.com>
2023-02-23 11:14:43 +0100

    store secrets as environment variables, style changes

--- backend/models.py
+++ backend/models.py
@@ -3,26 +3,22 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = 'trivia'
-database_path = 'postgresql://{}/{}'.format('localhost:5432', database_name)
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'trivia')
+DB_PATH = 'postgresql://{}@{}/{}'.format(DB_USER, DB_HOST, DB_NAME)
 
 db = SQLAlchemy()
 
-"""
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-"""
-def setup_db(app, database_path=database_path):
+
+def setup_db(app, database_path=DB_PATH):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
-"""
-Question
 
-"""
 class Question(db.Model):
     __tablename__ = 'questions'
 
@@ -58,10 +54,7 @@ class Question(db.Model):
             'difficulty': self.difficulty
             }
 
-"""
-Category
 
-"""
 class Category(db.Model):
     __tablename__ = 'categories'
 
